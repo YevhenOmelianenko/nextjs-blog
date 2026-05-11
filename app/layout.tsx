a28ecc40e5b91d67c7657381900e5ddad6f89029
YevhenOmelianenko <yevhen.omelianenko1@gmail.com>
2026-05-11 21:48:12 +0300

    add brandbook and basic styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '@/styles/globals.scss';
 import { HeaderMenu } from '@/components/header-menu';
 import { FooterMenu } from '@/components/footer-menu';
 
